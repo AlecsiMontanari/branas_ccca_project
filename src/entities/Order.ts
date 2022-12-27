d6e4d2c8c25dc29b7922a3ac7ce1c6a27c2e4400
Alecsi Montanari <alecsi.montanari@payface.com.br>
2022-12-27 18:23:40 -0300

    feature: adding order creation logic

--- src/entities/Order.ts
+++ src/entities/Order.ts
@@ -1,5 +1,36 @@
+import { validateCpf } from "../cpfValidator/cpfAfter";
+import { Coupon } from "./Coupon";
+import { OrderItem } from "./OrderItem";
+
 export class Order {
-    constructor() {
-        
+  private readonly orderNumber: string;
+  private readonly items: OrderItem[];
+  private readonly coupon?: Coupon;
+  private readonly customerCpf:string;
+  private itemsTotalAmount: number = 0;
+  private shippingAmount: number = 0;
+  private orderAmount: number = 0;
+
+  constructor(orderNumber: string, customerCpf:string, items?: OrderItem[], coupon?: Coupon) {
+    
+    if(!validateCpf(customerCpf)) throw new Error("Invalid CPF for order");
+    
+    
+    this.orderNumber = orderNumber;
+    this.items = items || [];
+    this.coupon = coupon;
+    this.customerCpf = customerCpf;
+  }
+
+  public calculateTotalAmount() {
+    for (let item of this.items) {
+      this.itemsTotalAmount += item.price;
+      this.shippingAmount += item.shippingPrice;
     }
+    this.orderAmount = this.itemsTotalAmount + this.shippingAmount;
+  }
+
+  public getorderAmount() {
+    return this.orderAmount;
+  }
 }
--- test/orders/order.test.ts
+++ test/orders/order.test.ts
@@ -1,21 +1,24 @@
 import { Order } from "../../src/entities/Order";
 
+test("Should create an Order", () => {
+  const order = new Order('1', '32557894869');
+  expect(order).toBeInstanceOf(Order);
+});
+
 test("Should not create an Order with invalid CPF", () => {
   expect(() => {
-    throw new Error("error");
+    const order = new Order('1', '32557894819');
   }).toThrowError();
 });
 
-test("Should create and Order with 3 items(description, price and quantity", () => {
+test("Should create and Order with 3 items(description, price and quantity)", () => {
   expect(() => {
-    const order = new Order();
     return true;
   }).toBeTruthy();
 });
 
 test("Should create an order with coupon discount (percentual)", ()=>{
     expect(() => {
-        const order = new Order();
         return true;
       }).toBeTruthy();
 })
